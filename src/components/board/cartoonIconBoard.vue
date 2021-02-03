8c74f0d4eab8b427cd03354e764dc6784e3161bb
yqf <1226283409@qq.com>
2021-02-03 18:16:47 +0800

    :lipstick: 新增字体图标

---
+++ src/components/board/cartoonIconBoard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+export default defineComponent({
+  setup() {
+    const icons = ref([1, 1]);
+    console.log(icons.value);
+    return {
+      icons
+    };
+  }
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   { path: '/clock', component: () => import('components/date/cartoonClock.vue') },
+  { path: '/cartoon-icon-board', component: () => import('components/board/cartoonIconBoard.vue') },
   { path: '/', component: () => import('@/views/test.vue') }
 ];
 const router = createRouter({
